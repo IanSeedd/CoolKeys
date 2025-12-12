0f9824ddc35b2059695c5b564f64ed1de19d237c
IAN LAMARTINE LOPES IANNACCONI <ian.iannacconi@aluno.senac.rio>
2025-12-12 20:33:35 -0300

    categoria-destaque

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=100, unique=True)
     descricao = models.TextField(blank=True, null=True)
+    destaque = models.BooleanField(default=False)
     
     def __str__(self):
         return self.nome
